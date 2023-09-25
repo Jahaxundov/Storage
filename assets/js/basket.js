cc6739dfdab71d2a0477b92eda958e0351651877
Jahaxundov <jahangirea@code.edu.az>
2023-09-26 00:26:48 +0400

    added

--- assets/js/basket.js
+++ assets/js/basket.js
@@ -5,48 +5,42 @@ let table = document.querySelector(".table-area")
 let basket = JSON.parse(localStorage.getItem("basket"))
 
 function getItems() {
-    
-    
-        for (const file of basket) {
 
-
-            tableBody.innerHTML+= `<tr id=${file.id}>
+    for (const file of basket) {
+        tableBody.innerHTML += `<tr id=${file.id}>
             <td>${file.name}</td>
             <td>${file.description}</td>
             <td><img src="${file.image}"></td>
             <td>${file.count}</td>
             <td><button>Delete</button></td>
-            </tr>`  
-        }
-        
+            </tr>`
+    }
+
 }
 
 getItems()
 
 
+let deleteBtn = document.querySelectorAll(".table-area button")
 
-let deleteBtn = document.querySelectorAll(".table-area button") 
+for (let btn of deleteBtn) {
+    btn.addEventListener('click', function () {
+        const id = this.parentElement.parentElement.id
+        let filtered = basket.filter(item => item.id !== parseInt(id))
+        localStorage.setItem('basket', JSON.stringify(filtered))
+        basketCount()
+        location.reload()
+    })
+}
+function basketCount() {
 
-for(let btn of deleteBtn) {
-   btn.addEventListener('click',function() {
-       const id = this.parentElement.parentElement.id
-       let filtered = basket.filter(item => item.id !== parseInt(id))
-       localStorage.setItem('basket',JSON.stringify(filtered))  
-       basketCount()
-       location.reload()
-   })
+    document.querySelector(".basket .count span").innerHTML = basket.length
 }
 
-    
-            function basketCount() {
-            
-            document.querySelector(".basket .count span").innerHTML = basket.length
-        }
-
-if (tableBody.innerText=="") {
+if (tableBody.innerText == "") {
     table.classList.add("d-none")
 }
-        
-       
 
-    
+
+
+
